373b71c69bb985478a1f42f4236c0b5fdd3e9161
Maxi <>
2020-10-06 17:26:05 -0300

    Modifique el metodo derivar Persona

--- TallerPOO/TallerPOO/src/tallerpoo/Hospital.java
+++ TallerPOO/TallerPOO/src/tallerpoo/Hospital.java
@@ -15,7 +15,7 @@ public class Hospital {
     private CM[] cm = new CM[5];
     private CTI[] cti = new CTI[2];
     private Aislamiento aislamiento;
-    private int contcm = 0, contcti = 0, contContagiados = 0;
+    private int contcm = 0, contcti = 0;
 
     public Hospital() {
         for (int i = 0; i < cm.length; i++) {
@@ -27,22 +27,21 @@ public class Hospital {
         this.aislamiento = new Aislamiento();
     }
 
-    public void derivarPersona() {
-        if (enfermo.mostrarContagiados().get(contContagiados).getComorbilidad().getEnfCard() || enfermo.mostrarContagiados().get(contContagiados).getComorbilidad().getEnfPul()) {
+    public void derivarPersona(Persona p) {
+        if (p.getComorbilidad().getEnfCard() || p.getComorbilidad().getEnfPul()) {
             if (!(contcti >= cti.length)) {
-                cti[contcti].setPaciente(enfermo.mostrarContagiados().get(contContagiados));
+                cti[contcti].setPaciente(p);
                 contcti++;
             }
         }
         if (!(contcti >= cti.length)) {
-            if (contcm >= cm.length && (enfermo.mostrarContagiados().get(contContagiados).getComorbilidad().getDiabetes() || enfermo.mostrarContagiados().get(contContagiados).getComorbilidad().getHipertencionArterial() || enfermo.mostrarContagiados().get(contContagiados).getComorbilidad().getObesidad())) {
-                cm[contcm].setPaciente(enfermo.mostrarContagiados().get(contContagiados));
+            if (contcm >= cm.length && (p.getComorbilidad().getDiabetes() || p.getComorbilidad().getHipertencionArterial() || p.getComorbilidad().getObesidad())) {
+                cm[contcm].setPaciente(p);
                 contcm++;
             } else {
-                this.aislamiento.setPaciente(enfermo.mostrarContagiados().get(contContagiados));
+                this.aislamiento.setPaciente(p);
             }
         }
-        contContagiados++;
     }
 
     public CTI[] mostrarPacientesCTI() {
