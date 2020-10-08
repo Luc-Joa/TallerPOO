0a99d1b47360826f9aa037f6759538bf15d4095d
Maxi <>
2020-10-08 20:49:42 -0300

    Agregue metodo consulta y agreagarContagiado

--- TallerPOO/TallerPOO/src/tallerpoo/Hospital.java
+++ TallerPOO/TallerPOO/src/tallerpoo/Hospital.java
@@ -5,19 +5,24 @@
  */
 package TallerPoo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Joaquin
  */
 public class Hospital {
 
-    private Contagiado enfermo;
+    private Persona persona;
     private CM[] cm = new CM[5];
     private CTI[] cti = new CTI[2];
     private Aislamiento aislamiento;
+    private List<Persona> perCont;
     private int contcm = 0, contcti = 0;
 
     public Hospital() {
+        this.perCont = new ArrayList<>();
         for (int i = 0; i < cm.length; i++) {
             cm[i] = new CM(this);
         }
@@ -27,6 +32,31 @@ public class Hospital {
         this.aislamiento = new Aislamiento();
     }
 
+    /**
+     * devuelve si una persona está contagiada o no, si está contagiada la
+     * agrega a la lista de persona correspondiente
+     *
+     * @return contagio
+     */
+    public Boolean consulta() {
+        return persona.getContagio();
+    }
+
+    /**
+     * agrega persona contagiada a la lista y la deriva según su gravedad
+     *
+     * @param p
+     */
+    public void agregarContagiado(Persona p) {
+        this.perCont.add(p);
+        derivarPersona(p);
+    }
+
+    /**
+     * muestra una lista de contagiados
+     *
+     * @return List perCont
+     */
     /**
      * deriva a la persona contagiada determinando su gravedad y comorbilidades
      *
